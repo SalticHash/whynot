f298b7985a326cdeb85e6745a2ab2102131df747
SalticHash <saltichash@gmail.com>
2026-03-30 13:30:57 -0600

    Ya no hay camino que recorra tu pasado

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,17 +1,60 @@
-import { CustomForm, Observable, ModalFormData, ActionFormData } from "@minecraft/server-ui";
+import { ActionFormData, uiManager } from "@minecraft/server-ui";
 import { world, system, Player, Entity } from "@minecraft/server";
+import { MinecraftBlockTypes } from "@minecraft/vanilla-data";
 
-world.afterEvents.blockContainerOpened.subscribe((ev) => open_jumpscare(ev.openSource.entity as Player));
-world.afterEvents.entityContainerOpened.subscribe((ev) => open_jumpscare(ev.openSource.entity as Player));
+
+const blocks = [
+    "chest",
+    "barrel",
+    "shulker_box"
+]
+const entities = [
+    "villager_v2"
+]
+world.beforeEvents.playerInteractWithBlock.subscribe(ev => {
+    if (Math.random() > 0.125) return;
+
+    if (!ev.isFirstEvent) return;
+    let is_interactable = false;
+    for (let block of blocks) {
+        if (!ev.block.typeId.endsWith(block)) continue;
+        is_interactable = true;
+        break;
+    }
+    if (!is_interactable) return;
+
+    ev.cancel = true;
+    system.run(() => open_jumpscare(ev.player));
+});
+world.beforeEvents.playerInteractWithEntity.subscribe(ev => {
+    if (Math.random() > 0.125) return;
+
+    let is_interactable = false;
+    for (let entity of entities) {
+        if (!ev.target.typeId.endsWith(entity)) continue;
+        is_interactable = true;
+        break;
+    }
+    if (!is_interactable) return;
+
+    ev.cancel = true;
+    system.run(() => open_jumpscare(ev.player));
+});
 
 function open_jumpscare(player: Player | undefined) {
-  if (player == undefined) return;
-  if (player.typeId != "minecraft:player") return;
-  const form = new ActionFormData();
-  form.title("wiki_form:");
+    if (player == undefined) return;
+    if (player.typeId != "minecraft:player") return;
+    const form = new ActionFormData();
+    form.title("camino_recorra_tu_pasado");
+    let runId = system.runTimeout(() => uiManager.closeAllForms(player), 3140);
 
-  form.show(player);
-  player.playMusic("camino_recorra_tu_pasado");
-  // player.addTag("camino_recorra_tu_pasado");
-  // system.runTimeout(() => player.removeTag("camino_recorra_tu_pasado"), 200)
+
+    player.playMusic("camino_recorra_tu_pasado")
+    form.show(player)
+        .finally(() => {
+            system.run(() => {
+                player.stopMusic()
+            });
+            system.clearRun(runId);
+        });
 }
