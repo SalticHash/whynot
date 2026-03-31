8359ed452e8d6b51e93b729fd3b4c1d2cd5f4303
SalticHash <saltichash@gmail.com>
2026-03-31 16:55:48 -0600

    Added outline and Light Fruit

--- scripts/addons/light_fruit.ts
+++ scripts/addons/light_fruit.ts
@@ -1,4 +1,24 @@
+import { EntityInventoryComponent, ItemLockMode, ItemStack, Player, StartupEvent, system, world } from "@minecraft/server";
 
+export function startup(ev: StartupEvent) {
+    ev.itemComponentRegistry.registerCustomComponent("whynot:light_fruit", {
+        onConsume(ev) {
+            const player = ev.source as Player
+            if (!player || player.typeId != "minecraft:player" || !player.isValid) return
+            system.run(() => {
+                const container = player.getComponent(EntityInventoryComponent.componentId)?.container
+                if (!container) return
+
+                const light = new ItemStack("whynot:light", 1);
+                light.lockMode = ItemLockMode.inventory;
+                light.keepOnDeath = true;
+                if (container.contains(light)) return
+
+                player.addItem(light);
+            })
+        }
+    })
+}
 export function main() {
 
 }
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,8 +1,11 @@
-import { world } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 import { main as camino_recorra_tu_pasado } from "./addons/camino_recorra_tu_pasado";
-import { main as light_fruit } from "./addons/light_fruit";
+import { main as light_fruit, startup as light_fruit_startup } from "./addons/light_fruit";
 
 world.afterEvents.worldLoad.subscribe(ev => {
     camino_recorra_tu_pasado();
     light_fruit();
+})
+system.beforeEvents.startup.subscribe(ev => {
+    light_fruit_startup(ev);
 })
